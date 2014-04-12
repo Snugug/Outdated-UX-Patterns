82e686f739b974eac33a1b43f39daf2c96081fc2
Sam Richard <snugug@gmail.com>
2014-04-12 15:04:28 -0400

    Build Stuff

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,21 @@ var options = require('./.compass-options').compassOptions();
 var browserSync = require('browser-sync');
 var shell = require('gulp-shell');
 
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+
+var imagemin = require('gulp-imagemin');
+var svgmin = require('gulp-svgmin');
+
+var clean = require('gulp-clean');
+var deploy = require("gulp-gh-pages");
+
+var plumber = require('gulp-plumber');
+
+
 //////////////////////////////
 // Lint Task
 //////////////////////////////
@@ -50,6 +65,67 @@ gulp.task('server', function () {
     }
   });
 });
+
+//////////////////////////////
+// Usemin Task
+//////////////////////////////
+gulp.task('usemin', function() {
+  gulp.src('index.html')
+    .pipe(usemin({
+      css: [minifyCss(), 'concat'],
+      html: [minifyHtml({empty: true})],
+      js: [uglify(), rev()]
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('imagemin', function () {
+  gulp.src([
+    options.html + '/' + options.img + '/**/*',
+    '!' + options.html + '/' + options.img + '/**/*.svg'
+  ])
+    .pipe(imagemin())
+    .pipe(gulp.dest('build/' + options.img));
+});
+
+gulp.task('svgmin', function () {
+  gulp.src([
+    options.html + '/' + options.img + '/**/*.svg'
+  ])
+    .pipe(svgmin())
+    .pipe(gulp.dest('build/' + options.img));
+});
+
+//////////////////////////////
+// Clean Task
+//////////////////////////////
+gulp.task('clean', function () {
+  gulp.src(options.html + '/build')
+    .pipe(clean());
+});
+
+//////////////////////////////
+// Build Task
+//////////////////////////////
+gulp.task('build', ['clean', 'usemin', 'imagemin', 'svgmin']);
+
+//////////////////////////////
+// Deploy Task
+//////////////////////////////
+// gulp.task('deploy', function () {
+//   gulp.src(options.html + '/build/**/*')
+//     .pipe(shell([
+//       'git push origin :gh-pages'
+//     ]))
+//     .pipe(plumber({
+//       errorHandler: error
+//     }))
+//     .pipe(console.log('Hello World'));
+// });
+
+// var error = function (err) {
+//   console.log('Oh Noes!');
+// }
 
 //////////////////////////////
 // Default Task
